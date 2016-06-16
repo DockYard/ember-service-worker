2c714b7ef00b4e7b3cc4b1b5be0e7d4f3836de99
Marten Schilstra <mail@martenschilstra.nl>
2016-06-16 19:11:26 -0400

    Install a service worker

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs');
+var mergeTrees = require('broccoli-merge-trees');
+var Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-service-worker'
+  name: 'ember-service-worker',
+
+  postprocessTree: function (type, tree) {
+    if (type === 'all') {
+      var swjs = new Funnel('lib', { include: ['sw.js'] });
+      return mergeTrees([tree, swjs]);
+    }
+
+    return tree;
+  },
+
+  contentFor: function(type, config) {
+    if (type === 'body-footer' && config.environment !== 'test') {
+      return '<script>' + fs.readFileSync('./lib/registration.js') + '</script>';
+    }
+  }
 };
