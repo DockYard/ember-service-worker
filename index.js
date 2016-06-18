c93f1c546d45994966e9ed6d12ec4767f3c50af5
Marten Schilstra <mail@martenschilstra.nl>
2016-06-18 08:56:48 -0400

    Fix IIFE in load script

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ module.exports = {
                 (function() {
                   var serviceWorkers = [${pluginNames}];
                   ${functionBody}
-                });
+                })();
               </script>`;
     }
   },
