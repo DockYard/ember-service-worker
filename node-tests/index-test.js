8c6a3bc13e1defd8fee121597fb1f060de5b7cc6
Marten Schilstra <mail@martenschilstra.nl>
2016-06-29 10:05:12 -0400

    Be compatible with ancient Node.js versions

--- node-tests/index-test.js
+++ node-tests/index-test.js
@@ -3,20 +3,20 @@
 /* jscs: disable */
 'use strict';
 
-const addonIndex = require('../index');
-const assert = require('chai').assert;
+var addonIndex = require('../index');
+var assert = require('chai').assert;
 
 suite('Index');
 
 test('_findPluginsFor grabs all plugins from a project', () => {
-  let project = {
+  var project = {
     addons: [
       { name: 'one', pkg: { keywords: ['ember-addon'] } },
       { name: 'two', pkg: { keywords: ['ember-addon', 'ember-service-worker-plugin'] } }
     ]
   };
 
-  let addons = addonIndex._findPluginsFor(project);
+  var addons = addonIndex._findPluginsFor(project);
 
   assert.equal(addons.length, 1, 'It should find one addon');
   assert.equal(addons[0].name, 'two', 'The addon named "two" should be found');
