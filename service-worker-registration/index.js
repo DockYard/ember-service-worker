409025dc320dd7d221f2b4f685d2ae7f9090e63b
Robert Jackson <rwjblue@twitch.tv>
2016-07-17 22:28:36 -0400

    Enable customizable registration.
    Allows plugins to register success/error handlers to be called during
    the registration process.

---
+++ service-worker-registration/index.js
@@ -0,0 +1,43 @@
+let SUCCESS_HANDLERS = [];
+let ERROR_HANDLERS = [];
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('{{ROOT_URL}}sw.js', { scope: '{{ROOT_URL}}' })
+    .then(function(reg) {
+      let current = new Promise.resolve();
+
+      for (let i = 0; i < SUCCESS_HANDLERS.length; i++) {
+        current = current.then(function() {
+          return SUCCESS_HANDLERS[i](reg);
+        });
+      }
+
+      return current
+        .then(function() {
+          console.log('Service Worker registration succeeded. Scope is ' + reg.scope);
+        });
+    })
+    .catch(function(error) {
+      let current = new Promise.resolve();
+
+      for (let i = 0; i < ERROR_HANDLERS.length; i++) {
+        current = current.then(function() {
+          return ERROR_HANDLERS[i](error);
+        });
+      }
+
+      return current
+        .then(function() {
+          console.log('Service Worker registration failed with ' + error);
+        });
+    });
+}
+
+
+export function addSuccessHandler(func) {
+  SUCCESS_HANDLERS.push(func);
+}
+
+export function addErrorHandler(func) {
+  ERROR_HANDLERS.push(func);
+}
