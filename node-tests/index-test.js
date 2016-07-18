ddbcfbb7b243f79fb64f0ca4631b49fe0c60da3c
Marten Schilstra <mail@martenschilstra.nl>
2016-07-18 20:55:35 +0200

    Test _transpilePath

--- node-tests/index-test.js
+++ node-tests/index-test.js
@@ -5,10 +5,25 @@
 
 var addonIndex = require('../index');
 var assert = require('chai').assert;
+var fs = require('fs');
+var path = require('path');
+var helpers = require('broccoli-test-helpers');
+var makeTestHelper = helpers.makeTestHelper;
+
+var fixturePath = path.join(__dirname, 'fixtures');
+
+var transpilePath = makeTestHelper({
+  subject: addonIndex._transpilePath,
+  fixturePath: fixturePath
+});
+
+var pathFromEntry = function(entry) {
+  return path.join(entry.basePath, entry.relativePath);
+};
 
 suite('Index');
 
-test('_findPluginsFor grabs all plugins from a project', () => {
+test('#_findPluginsFor grabs all plugins from a project', function() {
   var project = {
     addons: [
       { name: 'one', pkg: { keywords: ['ember-addon'] } },
@@ -20,4 +35,30 @@ test('_findPluginsFor grabs all plugins from a project', () => {
 
   assert.equal(addons.length, 1, 'It should find one addon');
   assert.equal(addons[0].name, 'two', 'The addon named "two" should be found');
+});
+
+test('#_transpilePath transpiles a tree from given path', function() {
+  var project = {
+    pkg: { name: 'test-project' },
+    root: path.join(fixturePath, 'transpile-tree-test'),
+    treeGenerator: function(dir) { return dir; }
+  };
+
+  return transpilePath(project, 'a-path').then(function(results) {
+    var indexOfFile = results.files.indexOf('test-project/a-path/file.js');
+    assert.ok(indexOfFile >= 0, 'File is copied over');
+
+    var filePath = pathFromEntry(results.entries[indexOfFile]);
+    assert.equal(fs.readFileSync(filePath), 'var foo = 42;');
+  });
+});
+
+test('#_transpilePath returns only if the target path exists', function() {
+  var project = {
+    pkg: { name: 'test-project' },
+    root: path.join(fixturePath, 'transpile-tree-test'),
+    treeGenerator: function(dir) { return dir; }
+  };
+
+  assert.equal(addonIndex._transpilePath(project, 'b-path'), undefined);
 });
