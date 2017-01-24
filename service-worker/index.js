326fc0405eee3b9fd790fc75631363f67870d46e
Marten Schilstra <marten.schilstra@dockyard.com>
2017-01-24 19:22:44 +0100

    Remove the fetch event DSL

--- service-worker/index.js
+++ service-worker/index.js
@@ -1,36 +1,6 @@
 export const PROJECT_REVISION = '{{PROJECT_REVISION}}';
 export const VERSION = '{{BUILD_TIME}}';
 
-let FETCH_HANDLERS = [];
-
-self.addEventListener('fetch', function fetchEventListenerCallback(event) {
-  let resolver = function fetchResolver(resolve, reject, index) {
-    if (!index) {
-      index = 0;
-    }
-
-    if (index >= FETCH_HANDLERS.length) {
-      resolve(fetch(event.request));
-      return;
-    }
-
-    let handler = FETCH_HANDLERS[index];
-    let result = handler(event);
-
-    Promise.resolve(result)
-      .then(function (response) {
-        if (response) {
-          return resolve(response);
-        } else {
-          return resolver(resolve, reject, index + 1);
-        }
-      })
-      .catch(reject);
-  };
-
-  event.respondWith(new Promise(resolver));
-});
-
 self.addEventListener('install', function installEventListenerCallback(event) {
   return self.skipWaiting();
 });
@@ -39,6 +9,3 @@ self.addEventListener('activate', function installEventListenerCallback(event) {
   return self.clients.claim();
 });
 
-export function addFetchListener(handler) {
-  FETCH_HANDLERS.push(handler);
-}
