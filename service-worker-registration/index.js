fc515c527301fb36d5b133364652a25ee5a6e7a9
Michael Villander <nousvillander@gmail.com>
2018-03-30 18:17:04 -0300

    Fix for

--- service-worker-registration/index.js
+++ service-worker-registration/index.js
@@ -22,7 +22,7 @@ if ('serviceWorker' in navigator) {
     .catch(function(error) {
       let current = Promise.resolve();
 
-      for (let i = 0, len = SUCCESS_HANDLERS.length; i < len; i++) {
+      for (let i = 0, len = ERROR_HANDLERS.length; i < len; i++) {
         current = current.then(function() {
           return ERROR_HANDLERS[i](error);
         });
