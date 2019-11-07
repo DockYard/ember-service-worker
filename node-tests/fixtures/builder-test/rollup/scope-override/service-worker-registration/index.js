1971028c78b4217f6e997b6d7b4221873707220c
Matthew Irish <matthew@hashicorp.com>
2019-11-07 16:40:59 -0600

    test rollup output with serviceWorkerScope option

--- node-tests/fixtures/builder-test/rollup/scope-override/service-worker-registration/index.js
+++
@@ -1,8 +0,0 @@
-const SCOPE = '{{SERVICE_WORKER_SCOPE}}';
-const ROOT_URL = '{{ROOT_URL}}';
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}', { scope: SCOPE || ROOT_URL });
-};
-
-
