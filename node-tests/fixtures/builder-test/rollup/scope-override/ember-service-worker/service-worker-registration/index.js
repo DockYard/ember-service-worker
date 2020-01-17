2c8f461581b7a0adfcd61b4844c6f8d5f2d03856
Lisa Backer <lisa.backer@dockyard.com>
2020-01-17 12:44:46 -0500

    PR Feedback to strengthen test story

--- node-tests/fixtures/builder-test/rollup/scope-override/ember-service-worker/service-worker-registration/index.js
+++ node-tests/fixtures/builder-test/rollup/scope-override/ember-service-worker/service-worker-registration/index.js
@@ -1,8 +1,3 @@
-const SCOPE = '{{SERVICE_WORKER_SCOPE}}';
-const ROOT_URL = '{{ROOT_URL}}';
-let scopePassed = SCOPE !== 'undefined';
-let scope = scopePassed ? SCOPE : ROOT_URL;
-
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}', { scope });
+  navigator.serviceWorker.register('{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}', { scope: '{{SERVICE_WORKER_SCOPE}}' });
 };
