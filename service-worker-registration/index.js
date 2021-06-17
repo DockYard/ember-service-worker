458efcfee8effd99156a1f0000de18d3a48d049a
Hector Carrillo <carrfane@gmail.com>
2021-06-17 14:05:03 -0400

    que lo que?

--- service-worker-registration/index.js
+++ service-worker-registration/index.js
@@ -4,35 +4,7 @@ let SUCCESS_HANDLERS = [];
 let ERROR_HANDLERS = [];
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}', { scope: '{{ROOT_URL}}' })
-    .then(function(reg) {
-      let current = Promise.resolve();
-
-      for (let i = 0, len = SUCCESS_HANDLERS.length; i < len; i++) {
-        current = current.then(function() {
-          return SUCCESS_HANDLERS[i](reg);
-        });
-      }
-
-      return current
-        .then(function() {
-          console.log('Service Worker registration succeeded. Scope is ' + reg.scope);
-        });
-    })
-    .catch(function(error) {
-      let current = Promise.resolve();
-
-      for (let i = 0, len = ERROR_HANDLERS.length; i < len; i++) {
-        current = current.then(function() {
-          return ERROR_HANDLERS[i](error);
-        });
-      }
-
-      return current
-        .then(function() {
-          console.log('Service Worker registration failed with ' + error);
-        });
-    });
+  console.log('que lo que?');
 }
 
 
