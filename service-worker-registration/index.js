035e132a2ff249a3d750949e364416474b5abc79
Hector Carrillo <carrfane@gmail.com>
2021-06-21 14:42:58 -0400

    log ignoreRegistration

--- service-worker-registration/index.js
+++ service-worker-registration/index.js
@@ -4,7 +4,7 @@ let SUCCESS_HANDLERS = [];
 let ERROR_HANDLERS = [];
 
 if ('serviceWorker' in navigator) {
-  if ('{{IGNORE_REGISTRATION}}' === 'true') return;
+  console.log('{{IGNORE_REGISTRATION}}' === 'true')
 
   navigator.serviceWorker.register('{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}', { scope: '{{ROOT_URL}}' })
     .then(function(reg) {
