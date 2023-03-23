5e2418ceab792dad18febbe85c929d5da072cbed
Alex de Caso <a.decaso@lodgerin.com>
2023-03-23 11:59:25 +0100

    CHANGE: disabled sw registration when user agent contains prerender

--- service-worker-registration/index.js
+++ service-worker-registration/index.js
@@ -1,40 +1,46 @@
-export const PROJECT_REVISION = '{{PROJECT_REVISION}}';
+export const PROJECT_REVISION = "{{PROJECT_REVISION}}";
 
 let SUCCESS_HANDLERS = [];
 let ERROR_HANDLERS = [];
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}', { scope: '{{ROOT_URL}}' })
-    .then(function(reg) {
-      let current = Promise.resolve();
+if ("serviceWorker" in navigator) {
+  const isPrerenderAgent =
+    navigator.userAgent.toLowerCase().indexOf("prerender") > -1;
+  if (!isPrerenderAgent) {
+    navigator.serviceWorker
+      .register("{{ROOT_URL}}{{SERVICE_WORKER_FILENAME}}", {
+        scope: "{{ROOT_URL}}",
+      })
+      .then(function (reg) {
+        let current = Promise.resolve();
 
-      for (let i = 0, len = SUCCESS_HANDLERS.length; i < len; i++) {
-        current = current.then(function() {
-          return SUCCESS_HANDLERS[i](reg);
-        });
-      }
+        for (let i = 0, len = SUCCESS_HANDLERS.length; i < len; i++) {
+          current = current.then(function () {
+            return SUCCESS_HANDLERS[i](reg);
+          });
+        }
 
-      return current
-        .then(function() {
-          console.log('Service Worker registration succeeded. Scope is ' + reg.scope);
+        return current.then(function () {
+          console.log(
+            "Service Worker registration succeeded. Scope is " + reg.scope
+          );
         });
-    })
-    .catch(function(error) {
-      let current = Promise.resolve();
+      })
+      .catch(function (error) {
+        let current = Promise.resolve();
 
-      for (let i = 0, len = ERROR_HANDLERS.length; i < len; i++) {
-        current = current.then(function() {
-          return ERROR_HANDLERS[i](error);
-        });
-      }
+        for (let i = 0, len = ERROR_HANDLERS.length; i < len; i++) {
+          current = current.then(function () {
+            return ERROR_HANDLERS[i](error);
+          });
+        }
 
-      return current
-        .then(function() {
-          console.log('Service Worker registration failed with ' + error);
+        return current.then(function () {
+          console.log("Service Worker registration failed with " + error);
         });
-    });
+      });
+  }
 }
-
 
 export function addSuccessHandler(func) {
   SUCCESS_HANDLERS.push(func);
